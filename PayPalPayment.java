60eec48abfdda78b63c5aaef5b24b66576871d70
zarafatima-y <110126525+zarafatima-y@users.noreply.github.com>
2025-03-25 02:56:49 -0400

    Create PayPalPayment.java

---
+++ PayPalPayment.java
@@ -0,0 +1,5 @@
+class PayPalPayment {
+    public void payWithPaypal(String transactionID, double amount) {
+        System.out.println("Processing PayPal payment: " + transactionID + " for amount: " + amount);
+    }
+}

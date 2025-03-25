32bbbd5124441351271ced362a3f298f5344ed4b
zarafatima-y <110126525+zarafatima-y@users.noreply.github.com>
2025-03-25 02:55:41 -0400

    Create CreditCardPayment.java

---
+++ CreditCardPayment.java
@@ -0,0 +1,5 @@
+class CreditCardPayment {
+    public void payWithCredit(String transactionID, double amount) {
+        System.out.println("Processing credit card payment: " + transactionID + " for amount: " + amount);
+    }
+}

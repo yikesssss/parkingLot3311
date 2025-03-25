dd71c7935d6b00e83749a1ad6894f906d74283fa
zarafatima-y <110126525+zarafatima-y@users.noreply.github.com>
2025-03-25 03:41:03 -0400

    Update SuperManager.java

--- SuperManager.java
+++ SuperManager.java
@@ -1,12 +1,25 @@
-class SuperManager extends UserConcrete {
+class SuperManager extends ManagementTeamMember {
     private static SuperManager instance;
+
     private SuperManager() {
-        super("supermanager@example.com", "SuperManager", "adminPass", "Super Manager");
+        super("supermanager@example.com", "SuperManager", "adminPass");
     }
+
     public static SuperManager getInstance() {
         if (instance == null) {
             instance = new SuperManager();
         }
         return instance;
+    }
+
+    public void generateManagementAccount(String email, String username, String password) {
+        User newManager = new ManagementTeamMember(email, username, password);
+        Database.getInstance("users.csv").users.add(newManager);
+        try {
+            Database.getInstance("users.csv").update("users.csv");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("New Management Account Created: " + email);
     }
 }

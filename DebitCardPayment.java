3385a35e9ec503cef7b2fd6053306f895beb94ba
zarafatima-y <110126525+zarafatima-y@users.noreply.github.com>
2025-03-25 02:56:12 -0400

    Create DebitCardPayment.java

---
+++ DebitCardPayment.java
@@ -0,0 +1,5 @@
+class DebitCardPayment {
+    public void payWithDebit(String transactionID, double amount) {
+        System.out.println("Processing debit card payment: " + transactionID + " for amount: " + amount);
+    }
+}

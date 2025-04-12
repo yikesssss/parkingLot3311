58ef3a1d3a80adce8513aabf0e55b05324646506
zarafatima-y <110126525+zarafatima-y@users.noreply.github.com>
2025-04-11 21:47:44 -0400

    Update PayPalPayment.java

--- PayPalPayment.java
+++ PayPalPayment.java
@@ -1,5 +1,10 @@
+package com;
 class PayPalPayment {
     public void payWithPaypal(String transactionID, double amount) {
-        System.out.println("Processing PayPal payment: " + transactionID + " for amount: " + amount);
+		if (amount > 0) {
+			System.out.println("Processing PayPal payment: " + transactionID + " for amount: " + amount);
+		} else {
+			System.out.println("Cannot Process PayPal payment: " + transactionID + " for amount: " + amount + "because amont negative");
+		}
     }
 }

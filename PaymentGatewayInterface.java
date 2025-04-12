4b9adcd7da7009b382de98e303149eec5f8a1bcf
zarafatima-y <110126525+zarafatima-y@users.noreply.github.com>
2025-04-11 21:48:23 -0400

    Update PaymentGatewayInterface.java

--- PaymentGatewayInterface.java
+++ PaymentGatewayInterface.java
@@ -1,7 +1,9 @@
+package com;
 interface PaymentGateway {
     boolean processTransaction(String transactionID, double amount, String paymentType);
     void paymentConfirmation(String transactionID);
     void paymentFailure(String errorCode);
     boolean refundRequest(String transactionID);
     void notifyRefundIssued(String transactionID);
+
 }

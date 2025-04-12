276713c284af855aca688bff9b512df34a053df9
zarafatima-y <110126525+zarafatima-y@users.noreply.github.com>
2025-04-11 21:33:59 -0400

    Update CreditCardPayment.java

--- CreditCardPayment.java
+++ CreditCardPayment.java
@@ -1,3 +1,5 @@
+package com;
+
 class CreditCardPayment {
     public void payWithCredit(String transactionID, double amount) {
         System.out.println("Processing credit card payment: " + transactionID + " for amount: " + amount);

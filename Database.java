d0a073d9dfd737ee5612b3469d5e0df3b56004cd
zarafatima-y <110126525+zarafatima-y@users.noreply.github.com>
2025-04-11 21:34:54 -0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,3 +1,11 @@
+package com;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 class Database {
     private static Database instance;
     public ArrayList<User> users = new ArrayList<>();
@@ -19,40 +27,50 @@ class Database {
         return instance;
     }
 
+	public static void resetInstance() {
+		instance = null;
+	}
+
     public void load(String path) throws Exception {
-        CsvReader reader = new CsvReader(path);
-        reader.readHeaders();
-        
-        while (reader.readRecord()) {
-            User user = new User();
-            user.setName(reader.get("name"));
-            user.setId(Integer.parseInt(reader.get("id")));
-            user.setEmail(reader.get("email"));
-            user.setPassword(reader.get("password"));
-            users.add(user);
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        String line;
+
+        // Skip header
+        reader.readLine();
+
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(",");
+            if (parts.length == 4) {
+                String email = parts[0];
+                String username = parts[1];
+                String password = parts[2];
+                String userType = parts[3];
+
+                User user = new UserConcrete(email, username, password, userType);
+                users.add(user);
+            }
         }
         reader.close();
     }
 
     public void update(String path) throws Exception {
-        try {
-            CsvWriter csvOutput = new CsvWriter(new FileWriter(path, false), ',');
-            csvOutput.write("name");
-            csvOutput.write("id");
-            csvOutput.write("email");
-            csvOutput.write("password");
-            csvOutput.endRecord();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path, false));
 
-            for (User u : users) {
-                csvOutput.write(u.getName());
-                csvOutput.write(String.valueOf(u.getId()));
-                csvOutput.write(u.getEmail()); 
-                csvOutput.write(u.getPassword());
-                csvOutput.endRecord();
+       
+        writer.write("email,username,password,userType");
+        writer.newLine();
+
+        for (User u : users) {
+            if (u instanceof UserConcrete uc) {
+                String line = uc.getEmail() + "," +
+                              uc.getUsername() + "," +
+                              uc.getPassword() + "," +
+                              uc.getUserType();
+                writer.write(line);
+                writer.newLine();
             }
-            csvOutput.close();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+
+        writer.close();
     }
 }

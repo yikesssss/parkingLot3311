f76349d440bdc33f587d43958f455f3eeaeaf2d3
zarafatima-y <110126525+zarafatima-y@users.noreply.github.com>
2025-04-11 21:53:04 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,6 +1,11 @@
+package com;
+
 interface User {
     void register();
     boolean login(String email, String password);
     boolean validateEmail();
     boolean validatePassword();
+
+    String getEmail();
+    String getPassword();
 }

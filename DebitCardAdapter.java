d26a6cbbb259875a241c6aaca043e6e746050704
zarafatima-y <110126525+zarafatima-y@users.noreply.github.com>
2025-04-11 21:39:20 -0400

    Update DebitCardAdapter.java

--- DebitCardAdapter.java
+++ DebitCardAdapter.java
@@ -1,29 +1,38 @@
+package com;
 class DebitCardAdapter implements PaymentGateway {
-    private DebitCardPayment debitCardPayment;
-    
+   protected DebitCardPayment debitCardPayment;
+
     public DebitCardAdapter() {
         this.debitCardPayment = new DebitCardPayment();
     }
-    
-    public boolean processTransaction(String transactionID, double amount, String paymentType) {
+
+    @Override
+	public boolean processTransaction(String transactionID, double amount, String paymentType) {
         debitCardPayment.payWithDebit(transactionID, amount);
         return true;
     }
-    
-    public void paymentConfirmation(String transactionID) {
+
+    @Override
+	public void paymentConfirmation(String transactionID) {
         System.out.println("Debit Card Payment Confirmed: " + transactionID);
     }
-    
-    public void paymentFailure(String errorCode) {
+
+    @Override
+	public void paymentFailure(String errorCode) {
         System.out.println("Debit Card Payment Failed: Error " + errorCode);
     }
-    
-    public boolean refundRequest(String transactionID) {
+
+    @Override
+	public boolean refundRequest(String transactionID) {
         System.out.println("Processing Debit Card Refund for: " + transactionID);
         return true;
     }
-    
-    public void notifyRefundIssued(String transactionID) {
+
+    @Override
+	public void notifyRefundIssued(String transactionID) {
         System.out.println("Refund Issued for Debit Card Transaction: " + transactionID);
     }
-}
+
+
+	}
+
